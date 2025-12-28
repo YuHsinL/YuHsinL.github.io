15b01f4cc13e4d2d6e148e49c6a3e9d8e5be7ecf
Cynthia <cynthialiu0310@gmail.com>
2025-12-28 10:19:16 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hi! I'm Cynthia.</h1>
+        <p>Scroll down to get to know me.</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
